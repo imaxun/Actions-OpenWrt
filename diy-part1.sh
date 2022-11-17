d7fe6fbb9089ccbab34a280ddd6c96ce722f9218
imaxun <imaxun@126.com>
2022-11-17 16:00:05 +0800

    修改diy配置

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,25 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+
+#1.配置软件包源#
+echo 'src-git kenzo  https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git Lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.default
+echo 'src-git small  https://github.com/kenzok8/small' >>feeds.conf.default
+
+
+#3.配置插件#
+mkdir -p package/custom
+cd package/custom
+
+git clone --depth=1 https://github.com/zzsj0928/luci-app-pushbot                #推送机器人
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome       #去广告
+git clone --depth=1 https://github.com/pymumu/openwrt-smartdns                  #选择Network > smartdns
+git clone --depth=1 https://github.com/vernesong/OpenClash                      #翻墙
+git clone --depth=1 https://github.com/fw876/helloworld                         #翻墙
+
+#3.配置主题#
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git
+
+
--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,16 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+
+sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+#修改默认网关
+sed -i 's/192.168.1.1/172.16.1.1/g' package/base-files/files/bin/config_generate
+#修改主机名
+sed -i 's/OpenWrt/LEDE/g' package/base-files/files/bin/config_generate
+#修改默认密码
+sed -i 's/root::0:0:99999:7:::/root:$1$jhtxo0rP$rQhY1u5GZLxMmlmRJPvzQ0:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+#修改默认wifi以及wifi密码
+sed -i '/set wireless.default_radio${devidx}.encryption=none/a\                        set wireless.default_radio${devidx}.key=imaxun112233' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/set wireless.default_radio${devidx}.encryption=none/set wireless.default_radio${devidx}.encryption=psk-mixed/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/wireless.default_radio${devidx}.ssid=OpenWrt/wireless.default_radio${devidx}.ssid=imaxun/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
